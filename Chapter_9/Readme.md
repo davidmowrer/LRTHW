7343a3ad64749cb9fb69e8d283d480f5e98c114a
David Mowrer <david.mowrer@comcast.net>
2014-06-29 11:44:03 -0600

    Commited Chapter_9

---
+++ Chapter_9/Readme.md
@@ -0,0 +1,8 @@
+I finally made one error on this assignment.  Mis-spelled a variable when trying to use it.
+Rubocop errors:
+Two for Line is too long. [84/80]
+
+Extra Credit:
+
+Q) Can you get the text "Here are the days: " and the days to print on the same line?
+If you use print instead of puts. Out side the string enter ,"\n" to advance a line.
---
+++ Chapter_9/ex9.rb
@@ -0,0 +1,23 @@
+# Prints out the text string
+# Here's is some new strange stuff, remember type it exactly.
+
+# Assigns the value for the abreviation for the days of the week to variable "days".
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+# Assigns the value for the abreviation for the months Jan - Aug to the variable "months"
+months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug'
+
+# Prints out the text string on the first line.
+# Prints out the text for the variable "days" on the next line.
+print 'Here are the days:', days, "\n"
+# Prints out the text string on the first line.
+# Prints out the text for the variable "months" one per line
+puts 'Here are the months:', months
+
+# This Prints out the content of the text between the PARAGRAPH'S,
+# is this a special string!
+puts <<PARAGRAPH
+There is something going on here.
+With the paragraph thing
+We'll be able to type as much as we like.
+Even 4 lines if we want , or 5 or 6.
+PARAGRAPH
