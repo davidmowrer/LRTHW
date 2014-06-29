6316ed739fc9f88dc3093199cbc45829a3592d68
David Mowrer <david.mowrer@comcast.net>
2014-06-29 11:41:26 -0600

    Commited Chapter_7

---
+++ Chapter_7/Readme.md
@@ -0,0 +1,27 @@
+Extra Credit
+For these next few exercises, you will have the exact same extra credit.
+
+Go back through and write a comment on what each line does.
+Read each one backwards or out loud to find your errors.
+From now on, when you make mistakes write down on a piece of paper what kind of mistake you made.
+When you go to the next exercise, look at the last mistakes you made and try not to make them in this new one.
+Remember that everyone makes mistakes. Programmers are like magicians who like everyone to think they are perfect and never wrong, but it's all an act. They make mistakes all the time.
+
+I didn't have any errors after typing this in, did have some rubocop errors.
+One for Favor format over String#%, which is ok at this time.
+
+Extra Credit:
+1) Add comments to ex7.rb
+2) Read ex7.rb to one of you fellow students
+3) Feel free to document the mistakes you make in Evernote. 
+4) ...
+5) This is a lie, your instructor never makes mistakes. ;-)
+
+Q) What happened when you changed print to puts? Why?
+
+Q) What happened when you removed the last line of the program?
+
+The last puts prints a blank line.  If you replace print with puts you get:
+Cheese
+Burger
+Instead of CheeseBurger
---
+++ Chapter_7/ex7.rb
@@ -0,0 +1,44 @@
+# Writes out the text string
+puts 'Mary had a little lamb.'
+# Writes out the text string, and inserts the variable 'snow'
+puts "It's fleece was as %s." % 'snow'
+# Writes out the text string
+puts 'And everywhere that Mary went.'
+# Writes out 10 ..........
+puts '.' * 10 # What's that do?
+
+# Assigns the value of C to variable end1
+end1 = 'C'
+# Assigns the value of h to variable end2
+end2 = 'h'
+# Assigns the value of e to variable end3
+end3 = 'e'
+# Assigns the value of e to variable end4
+end4 = 'e'
+# Assigns the value of s to variable end5
+end5 = 's'
+# Assigns the value of e to variable end6
+end6 = 'e'
+# Assigns the value of B to variable end7
+end7 = 'B'
+# Assigns the value of u to variable end8
+end8 = 'u'
+# Assigns the value of r to variable end9
+end9 = 'r'
+# Assigns the value of g to variable end10
+end10 = 'g'
+# Assigns the value of e to variable end11
+end11 = 'e'
+# Assigns the value of r to variable end12
+end12 = 'r'
+
+# Notice we are using print instead of puts here.  Change it to puts
+# and see what happens.
+# Writes out the value for each variable does not go to next line like put
+print end1 + end2 + end3 + end4 + end5 + end6
+# Writes out the value for each variable
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+# Writes out a blank line
+puts
